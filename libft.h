05f8cc8aefa8c37d1b5870e1b24a2e4356560af3
Sergio Balcorta <sergio.shearing@gmail.com>
2018-07-10 00:29:34 -0700

    normed my libft

--- libft.h
+++ libft.h
@@ -26,17 +26,17 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-typedef struct  s_dblist
+typedef struct		s_dblist
 {
-    struct s_dblist    *next;
-    struct s_dblist    *last;
-    void        *content;
-}               t_dblist;
+	struct s_dblist	*next;
+	struct s_dblist	*last;
+	void			*content;
+}					t_dblist;
 
-t_dblist      		*ft_dblist_new(void);
-t_dblist      		*double_insert_after(t_dblist *current);
+t_dblist			*ft_dblist_new(void);
+t_dblist			*double_insert_after(t_dblist *current);
 char				*ft_fronttrim(char *string);
-void	            ft_dblist_free(t_dblist *list);
+void				ft_dblist_free(t_dblist *list);
 void				ft_dblist_bridge(t_dblist *first, t_dblist *second);
 
 int					get_next_line(const int fd, char **line);
